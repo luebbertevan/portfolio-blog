4e5f26140c46cc7b849169266ec66cdceab8cfff
luebbertevan <luebbertevan@gmail.com>
2025-09-27 16:53:20 -0400

    Home Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,21 @@
-import { BlogPosts } from 'app/components/posts'
+import { BlogPosts } from "app/components/posts";
 
 export default function Page() {
-  return (
-    <section>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
-      </h1>
-      <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
-      </p>
-      <div className="my-8">
-        <BlogPosts />
-      </div>
-    </section>
-  )
+	return (
+		<section>
+			<h1 className="mb-8 text-2xl font-semibold tracking-tighter">
+				Evan Luebbert
+			</h1>
+			<p className="mb-4">
+				{`I am a graduate of Colorado State University with a B.S. in computer science and a minor in mathematics. I recently moved to New York City I am looking for opportunities in the field of computer software to gain experience in a professional setting.
+
+I have always had a certain affinity for creative problem solving and the satisfaction that comes with it. I enjoy hobbies such as rock climbing, making music, table-top rpgs, and playing strategy games all of which require a particular diligence and the ability to think outside the box. As such, computer science gives me the opportunity to exercise these skills and direct them into a career path. I also love fostering kittens!
+
+I look forward to furthering my experience within the field of computer science!`}
+			</p>
+			<div className="my-8">
+				<BlogPosts />
+			</div>
+		</section>
+	);
 }
